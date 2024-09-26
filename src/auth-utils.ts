c66c66826493b784c903ba5378431ec5d2044df0
Tyson <deppravacion@gmail.com>
2024-09-26 07:34:49 -0500

    ADD: jsonWebToken and @types

--- src/auth-utils.ts
+++ src/auth-utils.ts
@@ -1,4 +1,10 @@
+import { User } from "@prisma/client";
 import bcrypt from "bcrypt";
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { z } from "zod";
+import { prisma } from "../prisma/db.setup";
+
 const saltRounds = 11;
 
 export const hashPassword = (password: string) => {
