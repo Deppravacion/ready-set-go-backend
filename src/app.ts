33a77492752cc7619329f95d180140aa1019b03c
Tyson <deppravacion@gmail.com>
2024-10-02 14:54:17 -0500

    ADD: Cors things

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,20 @@ import { storeController } from "./router/stores.router";
 const app = express();
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend's origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+app.options("*", cors());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.use(authController);
 app.use(storeController);
-app.use(cors);
 
 app.listen(3000);
