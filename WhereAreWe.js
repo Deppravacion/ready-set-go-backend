519ba2c1175f20bc3a0db40917576aa2f152c466
Tyson <deppravacion@gmail.com>
2024-11-10 20:10:27 -0600

    removed: todo file

--- WhereAreWe.js
+++
@@ -1,9 +0,0 @@
-//node packages sorted
-// get Users route created but has error. -- prisma.user.create() path not exist
-//The table `main.Store` does not exist in the current database./
-// added passowrd hash to the schema and prisma migration
-// next is to set up a basic seed file  9/24
-//we fixed teh schema up again and seems to be solid and matching the OG shape
-// we have a npm run seed script
-// hashed passwords in the DB
-//working on the auth rout now on Auth.5 video
